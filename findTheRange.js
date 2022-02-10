3ff82464465b3b46fbe8b36ae10d25e0ae9782d3
caseysugden <66505022+caseysugden@users.noreply.github.com>
2022-02-10 10:37:04 -0700

    Solved challenge

--- findTheRange.js
+++ findTheRange.js
@@ -2,3 +2,18 @@
 let array = [28, 43, -12, 30, 4, 0, -36]
 
 // Write your solution below:
+let smallestNum = array[0];
+let largestNum = array[0];
+
+for (i = 0; i < array.length; i++) {
+  if (array[i] < smallestNum) {
+    smallestNum = array[i];
+  }
+  
+  if (array[i] > largestNum) {
+    largestNum = array[i];
+  }
+}
+
+console.log(`${smallestNum}`)
+console.log(`${largestNum}`)
